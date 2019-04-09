dbd3674573fe741f68c81a9c08da730945dbc73c
maklud <markoni4@live.com>
2019-04-09 14:54:33 +0200

    add ImageList.js V1 of this app that displays list of 10 found images

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-//import ImageList from './ImageList';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization:
-          'Client-ID e6a7ffb3eefe026719627db5b55387ef2b2808534608d2b26654aab289237dc4'
-      }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
     });
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '15px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
