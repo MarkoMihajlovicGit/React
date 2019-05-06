5fad8bc549ab4545c7aa4cd086baef1c54cef4e8
maklud <markoni4@live.com>
2019-05-06 15:48:11 +0200

    add url based selection  add history.js  edit button works now

---
+++ streams/client/src/components/streams/StreamForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class StreamForm extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default StreamForm;
---
+++ streams/client/src/history.js
@@ -0,0 +1,4 @@
+//history package was installed automaticaly from react-router-dom!!!
+import { createBrowserHistory } from 'history';
+
+export default createBrowserHistory();
