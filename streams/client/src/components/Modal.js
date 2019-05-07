d6cc22f5fe8fb559ea560c3d8393288d8b40eec8
maklud <markoni4@live.com>
2019-05-07 16:32:55 +0200

    add edit stream  functionality add modal.js add reusable modal  add delete functionality

---
+++ streams/client/src/components/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+      <div
+        //stop bubbleing
+        onClick={e => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+        <div className="" />
+      </div>
+    </div>,
+    document.querySelector('#modal')
+  );
+};
+
+export default Modal;
