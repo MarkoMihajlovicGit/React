097e8641f26374f8b25598a7d284c2398fbcf0c0
maklud <markoni4@live.com>
2019-08-14 14:06:28 +0200

    style ColorBox

--- Colt/colors-palette-app/src/ColorBox.js
+++ Colt/colors-palette-app/src/ColorBox.js
@@ -5,9 +5,14 @@ export default class ColorBox extends Component {
   render() {
     const { name, background } = this.props;
     return (
-      <div style={{ background: background }} className="ColorBox">
-        <span>{name}</span>
-        <span>More</span>
+      <div style={{ background }} className="ColorBox">
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
