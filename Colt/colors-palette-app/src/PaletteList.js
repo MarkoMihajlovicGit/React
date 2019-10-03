12d18eecab79223ca5694372dedc5d1d159c5251
maklud <markoni4@live.com>
2019-10-03 12:52:55 +0200

    Start styling MiniPalette

--- Colt/colors-palette-app/src/PaletteList.js
+++ Colt/colors-palette-app/src/PaletteList.js
@@ -7,13 +7,10 @@ export default class PaletteList extends Component {
     const { palettes } = this.props;
     return (
       <div>
-        <MiniPalette></MiniPalette>
         <h1>React Colors</h1>
 
         {palettes.map(palette => (
-          <p key={palette.id}>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette}></MiniPalette>
         ))}
       </div>
     );
